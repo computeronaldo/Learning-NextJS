a2016efcc1dd6ab301eca9c11ef9a0e403fd43b9
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2023-10-20 12:42:33 +0530

    fixed accessing id query parameter

--- next06/src/app/api/todos/[id]/route.ts
+++ next06/src/app/api/todos/[id]/route.ts
@@ -2,9 +2,14 @@ import { NextResponse } from "next/server";
 
 const DATA_SOURCE_URL = "https://jsonplaceholder.typicode.com/todos";
 
-export const GET = async (request: Request) => {
-  const id = request.url.slice(request.url.lastIndexOf("/") + 1);
+type Props = {
+  params: {
+    id: string;
+  };
+};
 
+export const GET = async (request: Request, { params: { id } }: Props) => {
+  //   const id = request.url.slice(request.url.lastIndexOf("/") + 1);
   const res = await fetch(`${DATA_SOURCE_URL}/${id}`);
 
   const todo: Todo = await res.json();
