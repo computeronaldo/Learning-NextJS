0bd4dc9d50f7892334a2225f249bf4ced86d1c58
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2023-10-21 00:19:38 +0530

    added CORS middleware and tested on '/api/todos' route by setting appropriate response headers

--- next06/src/middleware.ts
+++ next06/src/middleware.ts
@@ -1,17 +1,31 @@
 import { NextResponse } from "next/server";
 
+const allowedOrigins =
+  process.env.NODE_ENV === "production"
+    ? ["https://www.yoursite.com", "https://yoursite.com"]
+    : ["http://localhost:3000"];
+
 export const middleware = (request: Request) => {
-  const regex = new RegExp("/api/*");
+  //   const regex = new RegExp("/api/*");
 
-  if (regex.test(request.url)) {
+  //   if (regex.test(request.url)) {
+  //   }
+  const origin = request.headers.get("origin");
+  console.log(origin);
+
+  if(origin && !allowedOrigins.includes(origin)) {
+    return new NextResponse(null, {
+        status: 400,
+        statusText: 'Bad Request',
+        headers: {
+            'Content-Type': 'text/plain'
+        }
+    })
   }
-
   console.log("Middleware");
   console.log(request.method);
   console.log(request.url);
 
-  const origin = request.headers.get("origin");
-  console.log(origin);
 
   return NextResponse.next();
 };
