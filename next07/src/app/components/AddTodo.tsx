9615a18f601a5d17ac807b6c6a2c0dd933482ed6
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2023-10-23 22:16:40 +0530

    Created AddTodo and Navbar component

---
+++ next07/src/app/components/AddTodo.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { ChangeEvent, useTransition, useState, FormEvent } from "react";
+import { useRouter, usePathname } from "next/navigation";
+
+const initState: Partial<Todo> = {
+  userId: 1,
+  title: "",
+};
+
+const AddTodo = () => {
+  const router = useRouter();
+  const pathName = usePathname();
+  const [isPending, startTransition] = useTransition();
+  const [isFetching, setIsFetching] = useState(false);
+  const [data, setData] = useState(initState);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setIsFetching(true);
+
+    const addTodo = async ({ userId, title }: Partial<Todo>) => {
+      const res = await fetch("http://127.0.0.1:3500/todos", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userId,
+          title,
+        }),
+      });
+
+      await res.json();
+    };
+
+    addTodo(data);
+
+    setIsFetching(false);
+    setData((prevData) => ({
+      ...prevData,
+      title: "",
+    }));
+
+    startTransition(() => {
+      if (pathName === "/add") {
+        router.push("/");
+      } else {
+        // Refresh the current route and fetch new data
+        // from the server without losing
+        // client-side browser or React state.
+        router.refresh();
+      }
+    });
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name;
+
+    setData((prevData) => {
+      return {
+        ...prevData,
+        [name]: e.target.value,
+      };
+    });
+  };
+
+  const content = (
+    <form onSubmit={handleSubmit} className="flex gap-2 items-center">
+      <input
+        type="text"
+        id="title"
+        name="title"
+        value={data.title}
+        onChange={handleChange}
+        className="text-2xl p-1 rounded-lg flex-grow w-full"
+        placeholder="New Todo"
+        autoFocus
+      />
+      <button
+        type="submit"
+        className="p-2 text-xl rounded-2xl text-black border-solid border-black border-2 max-w-xs bg-green-500 hover:cursor-pointer hover:bg-green-400"
+      >
+        Submit
+      </button>
+    </form>
+  );
+  return <>{content}</>;
+};
+
+export default AddTodo;
