d1a56c0be36adcf0a3a115db4bc560f0fb0351cf
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2023-10-27 02:13:40 +0530

    updated individual post page to fetch post info from remote source

--- next08/app/posts/[postId]/page.tsx
+++ next08/app/posts/[postId]/page.tsx
@@ -3,6 +3,7 @@ import { getPostByName, getPostsMeta } from "@/lib/posts";
 import { notFound } from "next/navigation";
 import getFormattedDate from "@/lib/getFormattedDate";
 import Link from "next/link";
+import "highlight.js/styles/github-dark.css";
 
 export const revalidate = 0;
 
