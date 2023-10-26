38f517ac7d43a9b3518c2fd4f4ac8e3d82b10dd2
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2023-10-27 02:14:12 +0530

    changed configurations and installed package to compile .mdx files

--- next08/next.config.js
+++ next08/next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        port: "",
+        pathname: "/computeronaldo/TestBlogs/main/images/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
